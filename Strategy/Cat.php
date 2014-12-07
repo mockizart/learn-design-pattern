41cb674b0a8f92a9be0b4e65ae91f12eb97457a7
mockiemockiz <rifkimuhammad89@gmail.com>
2014-12-08 05:34:17 +0700

    directory, abstract class

---
+++ Strategy/Cat.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mockie
+ * Date: 12/8/14
+ * Time: 5:25 AM
+ */
+
+namespace Strategy;
+
+
+class Cat extends AbstractAnimal {
+
+    public function eat()
+    {
+
+    }
+
+    public function drink()
+    {
+
+    }
+
+    public function sleep()
+    {
+
+    }
+
+    public function play()
+    {
+
+    }
+
+} 
