b3e800206f64ae3eddb98830f8bfd111fbfc0f12
mockiemockiz <rifkimuhammad89@gmail.com>
2014-12-08 05:12:16 +0700

    first

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mockie
+ * Date: 12/8/14
+ * Time: 4:54 AM
+ */
+
+if(!function_exists('classAutoLoader')){
+    function classAutoLoader($class){
+        $class=strtolower($class);
+        $dirs = ['strategy'];
+        foreach ($dirs as $val) {
+            $classFile=$_SERVER['DOCUMENT_ROOT'].'/'.$val.'/'.$class.'.php';
+            if(is_file($classFile) && !class_exists($class)) {
+                include $classFile;
+            }
+        }
+    }
+}
+
+spl_autoload_register('classAutoLoader');
