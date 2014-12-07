f42b067381f3cda7c513827bff5876e99694a6af
mockiemockiz <rifkimuhammad89@gmail.com>
2014-12-08 05:34:17 +0700

    directory, abstract class

---
+++ Strategy/AbstractAnimal.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mockie
+ * Date: 12/8/14
+ * Time: 5:20 AM
+ */
+
+namespace Strategy;
+
+
+abstract class AbstractAnimal {
+
+    abstract public function eat();
+
+    abstract public function drink();
+
+    abstract public function play();
+
+    abstract public function sleep();
+
+} 
